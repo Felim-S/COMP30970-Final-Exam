6db292b8e027baae196632afbbb6304a42f12eac
Felim Sullivan <felim.sullivan@gmail.com>
2025-12-17 13:36:12 +0000

    further-work-on-prime-reduction

---
+++ exam-questions/Problem_A_Prime_Reduction.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <stack>
+#include <unordered_map>
+#include <climits>
+#include <complex>
+#include <map>
+#include <set>
+using namespace std;
+
+vector<long long> eratosthenes(long long n){
+    vector<bool> isMarked(n+1, false);
+    vector<long long> primes;
+    
+    for(long long i = 2; i * i <= n; i++){
+        if(!isMarked[i]){
+            for(long long j = i * i; j <= n; j += i){
+                isMarked[j] = true;
+            }
+        }
+    }
+
+    for(long long i = 2; i <= n; i++){
+        if(!isMarked[i]){
+            primes.push_back(i);
+        }
+    }
+
+    return primes;
+}
+
+map<long long, long long> factor(long long N) {
+    vector<long long> primes;
+    primes = eratosthenes((sqrt(N+1)));
+    map<long long, long long> factors;
+    for(int i = 0; i < primes.size(); ++i){
+        long long prime = primes[i], power = 0;
+        while(N % prime == 0){
+            power++;
+            N /= prime;
+        }
+        if(power > 0){
+            factors[prime] = power;
+        }
+    }
+    if (N > 1) {
+        factors[N] = 1;
+    }
+    return factors;
+}
+
+bool isPrime(long long n) {
+    if(n < 2) { return false; }
+    if(n % 2 == 0) { return n == 2; }
+
+    for(long long i = 3; i * i <= n; i += 2){
+        if(n % i == 0) { return false; }
+    }
+
+    return true;
+}
+
+long long primeReduction(long long N){
+    if(isPrime(N)){
+        return N;
+    }
+    map<long long, long long> factors = factor(N);
+    N = 0;
+    for(long long i = 0; i < factors.size(); i++){
+        N += factors[i];
+        for(long long p = 0; p < factors[i]; p++){
+            N *= factors[i];
+        }
+    }
+    return primeReduction(N);
+}
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    while(true){
+        long long N;
+        cin >> N;
+
+        if(N == 4){ break; }
+
+        cout << primeReduction(N);
+
+    }
+
+    return 0;
+}

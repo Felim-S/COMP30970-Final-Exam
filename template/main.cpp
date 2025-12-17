a93feb6778eb45eedd5aaa9b65449c3567c01de3
Felim Sullivan <felim.sullivan@gmail.com>
2025-12-17 12:57:54 +0000

    initial-commit

---
+++ template/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <stack>
+#include <unordered_map>
+#include <climits>
+#include <complex>
+#include <map>
+#include <set>
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    
+
+    return 0;
+}
